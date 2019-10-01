c2fddc299fa8d4a396b8f8c7ffe5d11a78150e26
Dasmez <dasmez@mail.ru>
2019-10-01 20:42:17 +0300

    changes2 flat_list.py

--- flat_list.py
+++ flat_list.py
@@ -2,7 +2,7 @@ def flat_list(array):
     c = []
     for element in array:
         if type(element) is list:
-            c += flat_list(array[i])
+            c += flat_list(element)
         else:
-            c.append(array[i])
+            c.append(element)
     return c
