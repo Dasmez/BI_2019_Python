17a2761152f33437e43792ced259e5a50c0fd8b3
Dasmez <dasmez@mail.ru>
2019-10-24 23:46:02 +0300

    ads 4th homework

---
+++ Common words.py
@@ -0,0 +1,9 @@
+def checkio(first, second):
+    result = []
+    first = first.split(",")
+    second = second.split(",")
+    first = set(first)
+    second = set(second)
+    result = set.intersection(first, second)
+    a = sorted(result)
+    return ",".join(a)
